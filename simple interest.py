691a627fe1da9d811725f9abd38479c3cd15e704
pallavi2615 <79102735+pallavi2615@users.noreply.github.com>
2023-03-02 21:35:18 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,19 @@
+#Python3 program to find simple interest
+# principal amount, time and
+# rate of interest taken from user.
+
+def simple_interest(p,t,r):
+    print('The Principal is', p)
+    print('The time period is', t)
+    print('The rate of interest is', r)
+
+    si = (p * t * r)/100
+
+    print('The simple interest is', si)
+
+
+# Driver code
+P = int(input('Enter the principle amount :') )
+T = int(input('Enter the time period :') )
+R = int(input('Enter the rate of interest :') )
+simple_interest(P,T,R)
